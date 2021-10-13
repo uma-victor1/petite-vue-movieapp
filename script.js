c4ee85bfd1e67ac32af89e53b4fd10764091d3fb
Uma Victor <umavictor11@gmail.com>
2021-10-13 01:33:59 +0100

    add movies

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const app = reactive({
     results: "",
     async search(){
         const movieSearch = await fetch(`https://api.themoviedb.org/3/movie/${this.movieType}?api_key=fbb0c92eb459272c7afd323360fb1146`)
-        const res = await movieSearch.json()
-        console.log(res);
+        this.results = await movieSearch.json()
+        
+        console.log(results);
         console.log(this.movieType);
     }
 })
 
 createApp({app}).mount("#movieapp");
+
+// `url(https://image.tmdb.org/t/p/w500${result.backdrop_path})`
